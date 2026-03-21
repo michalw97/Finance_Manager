8151834015460ded093c6076681c174f09069e07
konta <kontakt@witekmichal.pl>
2026-03-21 10:19:23 +0100

    Add edit_transaction function

--- main.py
+++ main.py
@@ -38,3 +38,25 @@ def delete_transaction():
             found = True
     if not found:
         print("Transakcja nie istnieje!")
+
+def edit_transaction():
+    edit = int(input("Którą transakcję chcesz edytować?(ID): "))
+
+    for transaction in transactions:
+        if transaction["id"] == edit:
+            edit_choice = input("Którą wartość chcesz zedytować? (Całość - wpisz całość!)").strip().lower()
+            if edit_choice == "całość":
+                transaction["amount"] = float(input("Nowa kwota: "))
+                transaction["category_type"] = input("Nowa kategoria: ")
+                transaction["transaction_type"] = input("Nowy typ: ")
+                transaction["transaction_date"] = input("Nowa data: ")
+            elif edit_choice == "kwota":
+                transaction["amount"] = float(input("Nowa kwota: "))
+            elif edit_choice == "kategoria":
+                transaction["category_type"] = input("Nowa kategoria: ")
+            elif edit_choice == "typ":
+                transaction["transaction_type"] = input("Nowy typ: ")
+            elif edit_choice == "data":
+                transaction["transaction_date"] = input("Nowa data: ")
+            else:
+                print("Nie ma takiej wartości!")
