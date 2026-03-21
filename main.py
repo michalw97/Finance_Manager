d6d8423e413d97c66ee202e331391227105f32ca
konta <kontakt@witekmichal.pl>
2026-03-21 09:46:01 +0100

    Add delete_transaction and update show_all with id

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 transactions = []
 
 def add_transaction():
+    new_id = len(transactions) + 1
     amount = float(input("Podaj kwotę: "))
     category_type = input("Podaj kategorię: ")
     transaction_type = input("Podaj typ transakcji: ")
     transaction_date = input("Podaj datę: ")
 
     transaction = {
+        "id": new_id,
         "amount": amount,
         "category_type": category_type,
         "transaction_type": transaction_type,
@@ -18,8 +20,21 @@ def add_transaction():
 def show_all():
     for transaction in transactions:
         print(f"Twoje wszystkie transakcje:")
+        print(f"ID transakcji: {transaction["id"]}")
         print(f"Kwota: {transaction["amount"]}")
         print(f"Kategoria: {transaction["category_type"]}")
         print(f"Typ: {transaction["transaction_type"]}")
         print(f"Data: {transaction["transaction_date"]}")
         print("----------------------")
+
+def delete_transaction():
+    delete = int(input("Którą transakcję chcesz usunąć?(ID)(ta zmiana jest nieodwracalna!): "))
+
+    found = False
+
+    for transaction in transactions:
+        if transaction["id"] == delete:
+            transactions.remove(transaction)
+            found = True
+    if not found:
+        print("Transakcja nie istnieje!")
