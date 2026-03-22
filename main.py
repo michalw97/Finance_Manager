2a4e08c641b6b8a4a8c205b3167f02aaebddf8cb
konta <kontakt@witekmichal.pl>
2026-03-22 02:53:24 +0100

    Add error handling to add_transaction, delete_transaction, edit_transaction, (edit_transaction requires a thorough refactoring)

--- main.py
+++ main.py
@@ -16,25 +16,30 @@ def save_data():
 
 def add_transaction():
     new_id = len(transactions) + 1
-    amount = float(input("Podaj kwotę: ").strip())
-    category_type = input("Podaj kategorię: ").strip()
-    transaction_type = input("Podaj typ transakcji('wpłata'/'wydatek'): ").lower().strip()
-    transaction_date = input("Podaj datę: ").strip()
-    print("Dodano transakcje.")
-    print()
+    while True:
+        try:
+            amount = float(input("Podaj kwotę: ").strip())
+            category_type = input("Podaj kategorię: ").strip()
+            transaction_type = input("Podaj typ transakcji('wpłata'/'wydatek'): ").lower().strip()
+            transaction_date = input("Podaj datę: ").strip()
+            print("Dodano transakcje.")
+            print()
 
-    transaction = {
-        "id": new_id,
-        "amount": amount,
-        "category_type": category_type,
-        "transaction_type": transaction_type,
-        "transaction_date": transaction_date
-    }
+            transaction = {
+                "id": new_id,
+                "amount": amount,
+                "category_type": category_type,
+                "transaction_type": transaction_type,
+                "transaction_date": transaction_date
+            }
 
-    transactions.append(transaction)
+            transactions.append(transaction)
+            break
+        except ValueError:
+            print("Kwota musi być liczbą!")
 
 def show_all():
-    print(f"Twoje wszystkie transakcje:")
+    print(f"Twoje wszystkie transakcje:\n")
     for transaction in transactions:
         print()
         print(f"ID transakcji: {transaction['id']}")
@@ -45,56 +50,82 @@ def show_all():
         print("----------------------\n")
 
 def delete_transaction():
-    delete = int(input("Którą transakcję chcesz usunąć?(ID)(ta zmiana jest nieodwracalna!): "))
+    while True:
+        try:
+            delete = int(input("Którą transakcję chcesz usunąć?(ID)(ta zmiana jest nieodwracalna!): "))
 
-    found = False
+            found = False
 
-    for transaction in transactions:
-        if transaction["id"] == delete:
-            transactions.remove(transaction)
-            found = True
-            print("Transakcja usunięta.")
-            print()
+            for transaction in transactions:
+                if transaction["id"] == delete:
+                    transactions.remove(transaction)
+                    found = True
+                    print("Transakcja usunięta.")
+                    print()
+                    break
+            if not found:
+                print("Transakcja nie istnieje!")
+                continue
             break
-    if not found:
-        print("Transakcja nie istnieje!")
+        except ValueError:
+            print("ID musi być liczbą!")
 
+#all edit_transaction for refactoring
 def edit_transaction():
-    edit = int(input("Którą transakcję chcesz edytować?(ID): "))
+    while True:
+        try:
+            edit = int(input("Którą transakcję chcesz edytować?(ID): "))
 
-    for transaction in transactions:
-        if transaction["id"] == edit:
-            edit_choice = input("Którą wartość chcesz zedytować? (Całość - wpisz całość!): ").strip().lower()
-            if edit_choice == "całość":
-                transaction["amount"] = float(input("Nowa kwota: ").strip())
-                transaction["category_type"] = input("Nowa kategoria: ").strip()
-                transaction["transaction_type"] = input("Nowy typ('wpłata'/'wydatek'): ").lower().strip()
-                transaction["transaction_date"] = input("Nowa data: ").strip()
-                print("Zmieniono całość.")
-                print()
-                break
-            elif edit_choice == "kwota":
-                transaction["amount"] = float(input("Nowa kwota: ").strip())
-                print("Zmieniono kwotę.")
-                print()
-                break
-            elif edit_choice == "kategoria":
-                transaction["category_type"] = input("Nowa kategoria: ").strip()
-                print("Zmieniono kategorię.")
-                print()
-                break
-            elif edit_choice == "typ":
-                transaction["transaction_type"] = input("Nowy typ('wpłata'/'wydatek'): ").lower().strip()
-                print("Zmieniono typ.")
-                print()
-                break
-            elif edit_choice == "data":
-                transaction["transaction_date"] = input("Nowa data: ").strip()
-                print("Zmieniono datę.")
-                print()
-                break
-            else:
-                print("Nie ma takiej wartości!")
+            found = False
+
+            for transaction in transactions:
+                if transaction["id"] == edit:
+                    while True:
+                        edit_choice = input(
+                            "Którą wartość chcesz zedytować? (Całość - wpisz całość!): ").strip().lower()
+                        if edit_choice == "całość":
+                            transaction["amount"] = float(input("Nowa kwota: ").strip())
+                            transaction["category_type"] = input("Nowa kategoria: ").strip()
+                            transaction["transaction_type"] = input("Nowy typ('wpłata'/'wydatek'): ").lower().strip()
+                            transaction["transaction_date"] = input("Nowa data: ").strip()
+                            found = True
+                            print("Zmieniono całość.")
+                            print()
+                            break
+                        elif edit_choice == "kwota":
+                            transaction["amount"] = float(input("Nowa kwota: ").strip())
+                            found = True
+                            print("Zmieniono kwotę.")
+                            print()
+                            break
+                        elif edit_choice == "kategoria":
+                            transaction["category_type"] = input("Nowa kategoria: ").strip()
+                            found = True
+                            print("Zmieniono kategorię.")
+                            print()
+                            break
+                        elif edit_choice == "typ":
+                            transaction["transaction_type"] = input("Nowy typ('wpłata'/'wydatek'): ").lower().strip()
+                            found = True
+                            print("Zmieniono typ.")
+                            print()
+                            break
+                        elif edit_choice == "data":
+                            transaction["transaction_date"] = input("Nowa data: ").strip()
+                            found = True
+                            print("Zmieniono datę.")
+                            print()
+                            break
+                        else:
+                            print("Nie ma takiej wartośći!")
+                            continue
+                    break
+            if not found:
+                print("Nie ma takiej transakcji!")
+                continue
+            break
+        except ValueError:
+            print("ID/Kwota musi być liczbą!")
 
 def show_incomes():
     print(f"Twoje wszystkie WPŁATY:\n")
